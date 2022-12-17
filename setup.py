c898d911bf0e90dece91a857fee025666a1d7655
Atif Ali <atifalimd@gmail.com>
2022-12-17 05:36:08 +0000

    setup.py is updated

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 ## edit below variables as per your requirements -
 REPO_NAME = "REPO_NAME"
-AUTHOR_USER_NAME = "c17hawke"
+AUTHOR_USER_NAME = "Atif Ali Mohammed"
 SRC_REPO = "src"
 LIST_OF_REQUIREMENTS = []
 
@@ -14,11 +14,11 @@ setup(
     name=SRC_REPO,
     version="0.0.1",
     author=AUTHOR_USER_NAME,
-    description="A small package for DVC",
+    description="A small package for dvc",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-    author_email="sunny.c17hawke@gmail.com",
+    author_email="atifalimd@gmail.com",
     packages=[SRC_REPO],
     license="MIT",
     python_requires=">=3.6",
