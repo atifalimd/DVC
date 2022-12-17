2e3dd8df22aceccb97d023ef537979f11446739c
Atif Ali <atifalimd@gmail.com>
2022-12-17 08:09:21 +0000

    gitignore for data is updated

--- src/stage_01_get_data.py
+++ src/stage_01_get_data.py
@@ -34,7 +34,7 @@ def main(config_path):
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
-    args.add_argument("--config", "-c", default="configs/config.yaml")
+    args.add_argument("--config", "-c", default="configs\config.yaml")
     parsed_args = args.parse_args()
 
     try:
